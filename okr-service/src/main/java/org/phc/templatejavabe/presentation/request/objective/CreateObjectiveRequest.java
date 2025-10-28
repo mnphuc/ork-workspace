69cd7952cef2d7b5ece5b03a6b6e43c406801a89
PhucMN <phucmn@vissoft.vn>
2025-10-28 10:12:08 +0700

    fix not run because unit test error

--- okr-service/src/main/java/org/phc/templatejavabe/presentation/request/objective/CreateObjectiveRequest.java
+++ okr-service/src/main/java/org/phc/templatejavabe/presentation/request/objective/CreateObjectiveRequest.java
@@ -2,6 +2,7 @@ package org.phc.templatejavabe.presentation.request.objective;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
 
 public record CreateObjectiveRequest(
     @NotBlank String title,
